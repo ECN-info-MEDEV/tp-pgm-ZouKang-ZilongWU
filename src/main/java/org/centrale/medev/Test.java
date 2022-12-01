a83ea7439f3f6890aae2848e61cf4f1284226fca
KangZou <zoukang99@icloud.com>
2022-12-01 11:32:41 +0100

    add test for seuillage

--- src/main/java/org/centrale/medev/Test.java
+++ src/main/java/org/centrale/medev/Test.java
@@ -1,6 +1,7 @@
 package org.centrale.medev;
 
 import org.centrale.medev.util.ReadPGM;
+import org.centrale.medev.util.Seuillage;
 
 import java.io.IOException;
 
@@ -9,7 +10,8 @@ public class Test {
 
         //read image
         int data2D[][] = ReadPGM.ReadImage("./images/baboon.pgm");
-
+        //add test for Seuilage
+        int Seuillage[][] = org.centrale.medev.util.Seuillage.AfterThreshold("./images/baboon.pgm",20,40);
 
         for (int row = 0; row < data2D.length; row++) {
             for (int col = 0; col < data2D[0].length; col++) {
